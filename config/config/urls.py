b9cfce991ede3f58311732bb1b9360bac46dd13d
r-ofarrell <ryanof@pm.me>
2024-11-26 23:43:29 -0500

    Configured index url.

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from search.views import index
 
 urlpatterns = [
+    path("", index, name="index"),
     path("admin/", admin.site.urls),
 ]
