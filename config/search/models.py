f143d879363bc51213b6257a18a6ee411971c573
r-ofarrell <ryanof@pm.me>
2024-11-29 22:14:08 -0500

    Began drafting HTML and attempted to add templates.

--- config/search/models.py
+++ config/search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dob = models.DateTimeField("dob")
+
+class Estimate(models.Model):
+    pass
--- config/search/views.py
+++ config/search/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(
+        request, "search/index.html"
+    )
