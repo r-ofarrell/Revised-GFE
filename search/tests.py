7c73581b47fae24b2efd613eeed834f88c5f5869
r-ofarrell <ryanof@pm.me>
2024-12-06 13:19:00 -0500

    Added tests to ensure 200 status code for search and create pages.

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
-# Create your tests here.
+class SearchPageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+class CreateGFEPageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/create/")
+        self.assertEqual(response.status_code, 200)
