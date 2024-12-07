6afc62dc74a55856d0224c4bb89230c8fd7d1d51
r-ofarrell <ryanof@pm.me>
2024-12-07 16:39:50 -0500

    Registered models in admin.py for manipulation on admin site.

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Therapist, Service, Location, Estimate, EstimateDetails
+
+admin.site.register(Client)
+admin.site.register(Therapist)
+admin.site.register(Service)
+admin.site.register(Location)
+admin.site.register(Estimate)
+admin.site.register(EstimateDetails)
