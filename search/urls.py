576f0cd4d74e9b3d46330d311a2bad7b43e8ccd0
r-ofarrell <ryanof@pm.me>
2024-12-08 18:33:36 -0500

    Updated search/urls.py to include CreateNewClientView.

--- search/urls.py
+++ search/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from .views import SearchPageView, SearchResultsView, CreateGFEView
+from .views import SearchPageView, SearchResultsView, CreateGFEView, CreateNewClientView
 
 urlpatterns = [
     path("", SearchPageView.as_view(), name="search"),
     path("results/", SearchResultsView.as_view(), name="results"),
     path("create/", CreateGFEView.as_view(), name="create"),
+    path("create-client/", CreateNewClientView.as_view(), name="create_client"),
 ]
